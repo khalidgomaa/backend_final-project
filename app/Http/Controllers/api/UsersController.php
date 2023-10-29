4e7cc8c6b3a53d9abf2cb2e2ca6031bc3503ea19
MostafaAskar <askermostafa87@gmail.com>
2023-10-29 19:56:37 +0200

    update register

--- app/Http/Controllers/api/UsersController.php
+++ app/Http/Controllers/api/UsersController.php
@@ -3,11 +3,11 @@
 namespace App\Http\Controllers\api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\UsersRequest;
 use App\Models\User;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
 use Illuminate\Validation\ValidationException;
 use Laravel\Sanctum\Sanctum;
 
@@ -17,18 +17,8 @@ class UsersController extends Controller
 
 
 
-    public function register(Request $data)
+    public function register(UsersRequest $data)
     {
-        $validator = Validator::make($data->all(),[
-            'name' => ['required', 'string', 'max:255'],
-            'email' => ['required', 'string', 'email', 'max:255', 'unique:' . User::class],
-            'phone' => ['required', 'string', 'min:11'],
-            'password' => ['required'],
-
-        ]);
-        if($validator->fails()){
-            return response($validator->errors()->all(),422);
-        }
        $user= User::create([
             'name' => $data['name'],
             'phone' => $data['phone'],
