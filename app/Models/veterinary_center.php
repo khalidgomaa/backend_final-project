a747a636713a569a275d480095fd5b533f4b92ce
abdos016 <Abdosakrdev016@gmail.com>
2023-11-11 16:56:18 +0200

    update in vet center

--- app/Models/veterinary_center.php
+++ app/Models/veterinary_center.php
@@ -5,9 +5,14 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class veterinary_center extends Model
+class Veterinary_center extends Model
 {
     use HasFactory;
-    protected $fillable = ["name",	"street_address",	"governorate",	"logo",	"about"	,"license" ,	"open_at" , "close_at" ,  "tax_record"	, "commercial_record" ,	"user_id"	
-];
+    protected $fillable = [
+        "name",    "street_address",    "governorate",    "logo",    "about", "license",    "open_at", "close_at",  "tax_record", "commercial_record",    "user_id"
+    ];
+    public function doctors()
+    {
+        return $this->hasMany(Doctor::class);
+    }
 }
