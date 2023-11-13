e517565c6208098ac2024c19c7ddf91964530a13
abdos016 <Abdosakrdev016@gmail.com>
2023-11-13 12:48:53 +0200

    Update Veterinary_center.php

--- app/Models/Veterinary_center.php
+++ app/Models/Veterinary_center.php
@@ -8,9 +8,19 @@ use Illuminate\Database\Eloquent\Model;
 class Veterinary_center extends Model
 {
     use HasFactory;
-
+    protected $fillable = [
+        "name",    "street_address",    "governorate",    "logo",    "about", "license",    "open_at", "close_at",  "tax_record", "commercial_record", "user_id", "status"
+    ];
     public function doctors()
     {
         return $this->hasMany(Doctor::class);
+    }
+    public function appointment()
+    {
+        return $this->hasMany(Appointment::class);
+    }
+    public function user()
+    {
+        return $this->belongsTo(User::class, "id");
     }
 }
